cf5d121af1f29a071fe2c4107eb86762f6d6aa8b
Hieu Tan Duong <hio.tand@gmail.com>
2022-05-16 15:49:01 +0200

    deleted old token

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch').default;
 
 // add role names to this object to map them to group ids in your AAD tenant
 const roleGroupMappings = {
+    //please work
     'admin': '52a8c007-4975-45c5-a895-7a2593617884'
 };
 
